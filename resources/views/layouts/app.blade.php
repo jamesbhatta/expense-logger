e78061c7535d2e327d0ad1496cf488bc2018a332
manojbhatta <jmsbhatta@gmail.com>
2020-09-12 19:00:03 +0545

    refactor and livewire installation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,92 +1,17 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Styles -->
-    {{-- <link href="{{ mix('css/app.css') }}" rel="stylesheet"> --}}
-
-    {{-- Temp --}}
-    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/custom-forms.min.css') }}">
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css">
+    @include('layouts.partials.head')
     @stack('styles')
-    <style>
-        html {
-            line-height: 1.65;
-        }
-
-        .svg-icon {
-            display: inline-flex;
-            align-self: center;
-            fill: currentColor;
-        }
-
-        .svg-icon svg {
-            height: 1em;
-            width: 1em;
-        }
-
-        .svg-icon.svg-baseline svg {
-            top: .125em;
-            position: relative;
-        }
-
-        /* Focus:outline-none */
-        .focus:outline-none {
-            outline: none;
-        }
-
-    </style>
-
 </head>
 <body class="bg-gray-100 text-gray-900 h-screen antialiased leading-normal">
     <div id="app">
-        <nav class="bg-blue-900 shadow mb-8 py-6">
-            <div class="container mx-auto px-6 md:px-0">
-                <div class="flex items-center justify-center">
-                    <div class="mr-6">
-                        <a href="{{ url('/') }}" class="text-lg font-semibold text-gray-100 no-underline">
-                            {{ config('app.name', 'Laravel') }}
-                        </a>
-                    </div>
-                    <div class="flex-1 text-right">
-                        @guest
-                        <a class="no-underline hover:underline text-gray-300 text-sm p-3" href="{{ route('login') }}">{{ __('Login') }}</a>
-                        @if (Route::has('register'))
-                        <a class="no-underline hover:underline text-gray-300 text-sm p-3" href="{{ route('register') }}">{{ __('Register') }}</a>
-                        @endif
-                        @else
-                        <a class="text-gray-300 text-sm pr-4" href="{{ route('home') }}">Dashboard</a>
-                        <a class="text-gray-300 text-sm pr-4" href="{{ route('expenses.index') }}">Expenses</a>
-
-                        <span class="text-gray-300 text-sm pr-4">{{ Auth::user()->name }}</span>
-
-                        <a href="{{ route('logout') }}" class="no-underline hover:underline text-gray-300 text-sm p-3" onclick="event.preventDefault();
-                                    document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="hidden">
-                            {{ csrf_field() }}
-                        </form>
-                        @endguest
-                    </div>
-                </div>
-            </div>
-        </nav>
+        @include('layouts.partials.navbar')
 
         @yield('content')
-    </div>
 
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-    <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+    </div>
+    @include('layouts.partials.scripts')
     @stack('scripts')
 </body>
 </html>
---
+++ resources/views/layouts/partials/head.blade.php
@@ -0,0 +1,16 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<meta name="csrf-token" content="{{ csrf_token() }}">
+
+<title>@isset($title) {{ $title}} | @endisset {{ config('app.name') }}</title>
+
+<script src="{{ asset('js/app.js') }}" defer></script>
+
+{{-- <link href="{{ mix('css/app.css') }}" rel="stylesheet"> --}}
+
+<link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+<link rel="stylesheet" href="{{ asset('css/custom-forms.min.css') }}">
+<link rel="stylesheet" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css">
+<livewire:styles />
+<link rel="stylesheet" href="{{ asset('css/styles.css') }}">

91182979e4ef7cf214cb45c5525f9db18217e094
manojbhatta <jmsbhatta@gmail.com>
2020-10-27 07:45:34 +0545

    used laravel mix for compiling assets

--- resources/views/layouts/partials/head.blade.php
+++ resources/views/layouts/partials/head.blade.php
@@ -5,12 +5,6 @@
 
 <title>@isset($title) {{ $title}} | @endisset {{ config('app.name') }}</title>
 
-{{-- <script src="{{ asset('js/app.js') }}"></script> --}}
-
-{{-- <link href="{{ mix('css/app.css') }}" rel="stylesheet"> --}}
-
-<link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
-<link rel="stylesheet" href="{{ asset('css/custom-forms.min.css') }}">
-<link rel="stylesheet" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css">
+<link rel="stylesheet" href="{{ mix('/css/app.css') }}">
 @livewireStyles
-<link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+{{-- <link rel="stylesheet" href="{{ asset('css/styles.css') }}"> --}}
--- webpack.mix.js
+++ webpack.mix.js
@@ -14,7 +14,8 @@ require('laravel-mix-tailwind');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-   .postCss('resources/css/app.css', 'public/css')
+   // .postCss('resources/css/app.css', 'public/css')
+   .sass('resources/css/app.scss', 'public/css')
    .tailwind('./tailwind.config.js');
 
 if (mix.inProduction()) {
